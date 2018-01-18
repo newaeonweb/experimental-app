39d5566a6c17d3dcb508419cf809602d38522419
newaeonweb <fernando@newaeonweb.com.br>
2018-01-18 15:19:26 -0200

    added new directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,9 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { LoginModule } from './login/login.module';
+import { HomeModule } from './pages/home/home.module';
+import { AboutModule } from './pages/about/about.module';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
   imports: [
